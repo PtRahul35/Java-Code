7b6a82e9f599edf369dace497adb51afd3cf8316
Rahul Kumar <rahulkr3570@gmail.com>
2024-06-26 16:56:55 +0530

    first Commit

---
+++ PrintNumbers.java
@@ -0,0 +1,12 @@
+// How to print number 35, 70 , 140, 2800
+
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(35);
+        System.out.println(70);
+        System.out.println(140);
+        System.out.println(2800);
+    }
+    
+}
