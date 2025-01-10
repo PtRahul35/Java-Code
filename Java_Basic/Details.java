54c3c4a7467464ad53549ba17ec8b01861130a28
Rahul Kumar <rahulkr3570@gmail.com>
2025-01-10 22:59:09 +0530

    Add all my files

---
+++ Java_Basic/Details.java
@@ -0,0 +1,13 @@
+//Program to print your details
+
+public class Details {
+
+    public static void main (String [] args) {
+
+        System.out.println("Name: Rahul Kumar");
+        System.out.println("Class: ");
+        System.out.println("Section: ");
+        System.out.println("Roll No: ");
+        System.out.println("Date of Birth: ");
+    }
+}
