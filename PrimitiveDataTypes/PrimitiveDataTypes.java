7cbe81c827372c04ead4b960276884109b2a792a
Rahul Kumar <rahulkr3570@gmail.com>
2025-01-12 14:42:41 +0530

    added all files

---
+++ PrimitiveDataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+public class PrimitiveDataTypes {
+    
+    public static void main (String [] args) {
+
+            byte bValue = 1;
+            short Svalue = 10;
+            int IValue = 256;
+            long LValue = 100000L;
+            float FValue = 4.5f;
+            double DValue = 99.99;
+            char cValue = 'A';
+            boolean BValue = true; 
+
+            System.out.println("Value of Byte Variable: " +bValue);
+            System.out.println("Value of Short Variable: "+ Svalue);
+            System.out.println("Value of Int Variable: "+IValue);
+            System.out.println("Value of Long Variable: "+LValue);
+            System.out.println("Value of Float Variable: "+FValue);
+            System.out.println("Value of Double Variable: "+DValue);
+            System.out.println("Value of Char Variable: "+cValue);
+            System.out.println("Value of Boolean Variable: "+BValue);
+
+
+    }
+}
